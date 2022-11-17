9c2f524eddf102506afcc7955939ab3b1b895268
Wyllwyll <terry.boeuf@hotmail.fr>
2022-11-17 12:04:54 +0100

    maj

--- script.js
+++ script.js
@@ -9,26 +9,7 @@ shi.addEventListener('click', () => console.log('shi'));
 fu.addEventListener('click', () => console.log('fu'));
 mi.addEventListener('click', () => console.log('mi'));
 
-// Pour le reste, a vous de jouer
-
-
-
-
-function playerScore (playerChoice) {
-    if (shi.addEventListener('click', () => console.log('shi')) == 'shi') {
-        playerChoice = 'shi';
-    }
-    else if (fu.addEventListener('click', () => console.log('fu')) == 'fu') {
-        playerChoice = 'fu';
-    }
-    else if (mi.addEventListener('click', () => console.log('mi'))){
-        playerChoice = 'mi';}
-    else {
-        return playerChoice
-    }
-}
-console.log(playerScore())
-
+// Pour le reste, a vous de jouer 
 
 let arrayIaChoice = ["shi","fu","mi"];
 
@@ -36,6 +17,28 @@ function resultRandomIaChoice() {
     let randomIaChoice = arrayIaChoice[Math.floor(Math.random()*arrayIaChoice.length)] 
     return randomIaChoice
 }    
-
-
 //console.log(resultRandomIaChoice());
+
+
+function playerScore (playerChoice) {
+    if (shi.addEventListener('click', () => console.log('shi')) == 'shi') {
+        player.src = "/img/shi.png"; 
+        playerChoice = 'shi';
+    }
+    else if (fu.addEventListener('click', () => console.log('fu')) == 'fu') {
+        player.src="/img/fu.png";
+        playerChoice = 'fu';
+    }
+    else if (mi.addEventListener('click', () => console.log('mi'))){
+        player.src="/img/mi.png";
+        playerChoice = 'mi';}
+    else {
+        return playerChoice
+    }
+}
+//console.log(playerScore())
+
+
+
+
+
